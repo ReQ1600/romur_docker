2f0e49ac5c475a1e9a0f72721ec7afeed9d3c5bc
Mateusz Drogowski <drogowskimateusz8@gmail.com>
2025-11-18 22:45:11 +0100

    node repos now cloned into external folder and mounted, added gitignore

--- setup.sh
+++ setup.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
+docker build -t romur:latest .
+
+mkdir mt -p
+git clone git@github.com:ReQ1600/romur_driver_communication.git mt/romur_driver_communication
+git clone git@github.com:ReQ1600/romur_camera_publisher.git mt/romur_camera_publisher
+
+# gives permissions to mt to docker user
+sudo chown -R 69:69 "$(pwd)"/mt
+
 xhost +local:root && docker run -it \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -v ~/ros2:/root/ros2 \
+    --mount type=bind,src="$(pwd)"/mt,dst=/home/user/ros_workspace/src \
     --net=host \
     --privileged \
     --hostname romur \
     --name=romur \
     romur:latest
+
+sudo chmod +x start_container.sh
